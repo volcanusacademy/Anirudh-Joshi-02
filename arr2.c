d5c88fa96e09f2579d246c514c0acf5cf4170bfa
Anirudh joshi <yashjoshi5521@gmail.com>
2023-09-13 15:26:19 +0530

    BAsic programs Based on Arrays

---
+++ arr2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int arr[5]={1,2,3,4,5};
+    printf("%d",arr[0]);
+    printf("\n%d",arr[1]);
+    printf("\n%d",arr[2]);
+    printf("\n%d",arr[3]);
+    printf("\n%d",arr[4]);
+
+}
